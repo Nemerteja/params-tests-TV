8e2eda1b4dc64724c01ff9cecc3b32781b0e9dcf
Nemerteja <aagoncharenko.91@gmail.com>
2021-12-29 15:06:38 +0300

    fix sublect

--- src/test/java/testes/FirstTest.java
+++ src/test/java/testes/FirstTest.java
@@ -3,12 +3,12 @@ package testes;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
 import java.io.File;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selectors.byXpath;
 import static com.codeborne.selenide.Selenide.*;
 
 
@@ -39,7 +39,7 @@ public class FirstTest {
         $(byText("Music")).click();
 
         File file = new File("src/test/java/testes/resourses/1.png");
-       $("#uploadPicture").uploadFile(file);
+        $("#uploadPicture").uploadFile(file);
 
         $("#currentAddress").setValue("Some address");
         $(byText("Select State")).click();
@@ -49,16 +49,16 @@ public class FirstTest {
         $("#submit").click();
 
         $("#example-modal-sizes-title-lg").shouldBe(visible);
-        $("[class=\"modal-body\"]").shouldHave(text("Student Name"),text("Some Name"),
-                text("Student Email"),text("aaa@aa.aa"),
-                text("Gender"),text("Female"),
-                text("Mobile"),text("1234567891"),
-                text("Date of Birth"),text("25 April,2005"),
-                text("Subjects"),text("Maths"),
-                text("Hobbies"),text("Music"),
-                text("Picture"),text("1.png"),
-                text("Address"),text("Some address"),
-                text("State and City"),text("NCR Delhi"));
+        $("[class=\"modal-body\"]").shouldHave(text("Student Name"), text("Some Name"),
+                text("Student Email"), text("aaa@aa.aa"),
+                text("Gender"), text("Female"),
+                text("Mobile"), text("1234567891"),
+                text("Date of Birth"), text("25 April,2005"),
+                text("Subjects"), text("Maths"),
+                text("Hobbies"), text("Music"),
+                text("Picture"), text("1.png"),
+                text("Address"), text("Some address"),
+                text("State and City"), text("NCR Delhi"));
 
-}
+    }
 }
