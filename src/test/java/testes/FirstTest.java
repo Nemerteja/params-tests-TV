597a74b28527f3a727a964751b2969179577bf31
Nemerteja <aagoncharenko.91@gmail.com>
2021-12-30 04:43:03 +0300

    fix byText in most cases

--- src/test/java/testes/FirstTest.java
+++ src/test/java/testes/FirstTest.java
@@ -3,6 +3,7 @@ package testes;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 
 import java.io.File;
 
@@ -25,37 +26,37 @@ public class FirstTest {
         $("[id=firstName]").setValue("Some");
         $("[id=lastName]").setValue("Name");
         $("#userEmail").setValue("aaa@aa.aa");
-        $(byText("Female")).click();
+        $("[class=\"custom-control custom-radio custom-control-inline\"]").click();
         $("#userNumber").setValue("1234567891");
 
         $("#dateOfBirthInput").click();
-        $(byText("December")).click();
-        $(byText("April")).click();
-        $(byText("2021")).click();
-        $(byText("2005")).click();
-        $(byText("25")).click();
+        $("[class=\"react-datepicker__month-select\"]").click();
+        $("[value=\"3\"]").click();
+        $("[class=\"react-datepicker__year-select\"]").click();
+        $("[value=\"2005\"]").click();
+        $("[class=\"react-datepicker__day react-datepicker__day--025\"]").click();
         $("#subjectsInput").setValue("m");
         $(byText("Maths")).click();
-        $(byText("Music")).click();
+        $("[class=\"custom-control custom-checkbox custom-control-inline\"]").click();
 
         File file = new File("src/test/java/testes/resourses/1.png");
         $("#uploadPicture").uploadFile(file);
 
         $("#currentAddress").setValue("Some address");
-        $(byText("Select State")).click();
+        $("#state").click();
         $(byText("NCR")).click();
-        $(byText("Select City")).click();
+        $("#city").click();
         $(byText("Delhi")).click();
         $("#submit").click();
 
         $("#example-modal-sizes-title-lg").shouldBe(visible);
         $("[class=\"modal-body\"]").shouldHave(text("Student Name"), text("Some Name"),
                 text("Student Email"), text("aaa@aa.aa"),
-                text("Gender"), text("Female"),
+                text("Gender"), text("Male"),
                 text("Mobile"), text("1234567891"),
                 text("Date of Birth"), text("25 April,2005"),
                 text("Subjects"), text("Maths"),
-                text("Hobbies"), text("Music"),
+                text("Hobbies"), text("Sports"),
                 text("Picture"), text("1.png"),
                 text("Address"), text("Some address"),
                 text("State and City"), text("NCR Delhi"));
