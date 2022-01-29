f6a9b474a6072b1f63e19da77b1443d176c140b4
Nemerteja <aagoncharenko.91@gmail.com>
2022-01-29 15:56:50 +0300

    added Xls, xlsx file Tests

--- src/test/java/Utils/Files.java
+++ src/test/java/Utils/Files.java
@@ -5,8 +5,18 @@ import com.codeborne.xlstest.XLS;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.util.Iterator;
+
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 
 public class Files {
     public static String readTextFromFile(File file) throws IOException {
@@ -28,8 +38,41 @@ public class Files {
         return new PDF(getFile(path));
     }
 
-    public static XLS getXLS(String path){
+    public static XLS getXLS(String path) throws IOException {
 
         return new XLS(getFile(path));
     }
+
+    public static String readXlsxFromPath(String path) {
+        String result = "";
+        XSSFWorkbook myExcelBook = null;
+        try {
+            myExcelBook = new XSSFWorkbook(new FileInputStream(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        XSSFSheet myExcelSheet = myExcelBook.getSheetAt(0);
+        Iterator<Row> rows = myExcelSheet.iterator();
+        while (rows.hasNext()) {
+            org.apache.poi.ss.usermodel.Row row = rows.next();
+            Iterator<Cell> cells = row.iterator();
+            while (cells.hasNext()) {
+                org.apache.poi.ss.usermodel.Cell cell = cells.next();
+                CellType cellType = cell.getCellType();
+                switch (cellType) {
+                    default:
+                        result += cell.toString();
+                        break;
+                }
+            }
+        }
+        try {
+            myExcelBook.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+
 }
--- src/test/java/testes/XlsFileTest.java
+++ src/test/java/testes/XlsFileTest.java
@@ -3,10 +3,10 @@ package testes;
 import com.codeborne.xlstest.XLS;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
 import java.io.IOException;
 
 import static Utils.Files.getXLS;
+import static Utils.Files.readXlsxFromPath;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.StringContains.containsString;
 
@@ -17,7 +17,8 @@ public class XlsFileTest {
         String exepectedData = "some text";
 
         XLS sprxls = getXLS(xlsFilePath);
-        assertThat(sprxls, XLS.containsText(exepectedData));}
+        assertThat(sprxls, XLS.containsText(exepectedData));
+    }
 
     @Test
     public void xlsCellTest() throws IOException {
@@ -25,11 +26,20 @@ public class XlsFileTest {
         String exepectedData = "some text";
 
         XLS sprxls = getXLS(xlsFilePath);
-        String actData = sprxls.excel.getSheetAt(0).getRow(0).getCell(0).toString();
+        String actData = sprxls.excel.getSheetAt(0).getRow(2).getCell(1).toString();
         assertThat(actData, containsString(exepectedData));
 
     }
 
+    @Test
+    public void xlsxTest() throws IOException {
+        String xlsFilePath = "src/test/java/testes/resourses/2.xlsx";
+        String exepectedData = "some text";
+
+         String actData = readXlsxFromPath(xlsFilePath);
+        assertThat(actData, containsString(exepectedData));
+
     }
+}
 
 
