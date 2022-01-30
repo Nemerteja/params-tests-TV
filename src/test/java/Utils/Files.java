05ed3bb5103fcb68a9a56a8593941ade4e20ed4a
Nemerteja <aagoncharenko.91@gmail.com>
2022-01-30 15:50:32 +0300

    added doc file Tests

--- src/test/java/Utils/Files.java
+++ src/test/java/Utils/Files.java
@@ -9,13 +9,20 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
+import org.apache.hc.core5.util.TextUtils;
+import org.apache.poi.hwpf.HWPFDocument;
+import org.apache.poi.hwpf.extractor.WordExtractor;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.apache.poi.xwpf.usermodel.XWPFDocument;
+import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 
 
 public class Files {
@@ -75,4 +82,44 @@ public class Files {
     }
 
 
+    // Reading data from ".doc" file.
+    public static String readDocFile(String filePath) throws IOException
+    {
+        File file=new File(filePath);
+
+        FileInputStream fis =new FileInputStream(file);
+        HWPFDocument doc=new HWPFDocument(fis);
+
+        WordExtractor extractor=new WordExtractor(doc);
+        // Getting all the paragraphs from the document and adding the same in String array.
+        String[] getDocParagraphs= extractor.getParagraphText();
+        String str = String.join(",", getDocParagraphs);
+
+
+        extractor.close();
+        return str;
+    }
+
+    // Reading data from ".docx" file.
+    public static String readDocxFile(String filePath) throws IOException
+    {
+        File file=new File(filePath);
+
+        FileInputStream fis =new FileInputStream(file);
+        XWPFDocument docx =new XWPFDocument(fis);
+        List<XWPFParagraph> getDocxParagraphs= docx.getParagraphs();
+
+
+
+
+        String str = String.join(",", getDocxParagraphs);
+
+
+
+        docx.close();
+        return str;
+    }
 }
+
+
+
