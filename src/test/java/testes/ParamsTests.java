e16c9e0e68de13136cef4992726582b14e25f1fd
Nemerteja <aagoncharenko.91@gmail.com>
2022-02-06 16:19:01 +0300

    add ParamsTests

--- src/test/java/testes/ParamsTests.java
+++ src/test/java/testes/ParamsTests.java
@@ -2,8 +2,6 @@ package testes;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.*;
 
